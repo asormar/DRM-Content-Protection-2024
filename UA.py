8cbb3ec1d1126d0a80d9cd3562dc819ce2e1b570
Manuel Ricart <manuel.ricart.soler@gmail.com>
2025-01-04 17:40:48 +0100

    Entropia

--- UA.py
+++ UA.py
@@ -8,6 +8,8 @@ import base64
 import time
 import json
 import os
+from collections import Counter  # Import Counter for frequency calculation
+
 def leer_json(nombre_archivo_json):
     """
     Lee el archivo JSON y devuelve su contenido como un diccionario.
@@ -23,7 +25,9 @@ def leer_json(nombre_archivo_json):
     except json.JSONDecodeError:
         print(f"Error al leer el archivo JSON: {nombre_archivo_json}.")
         return None
+
 cifrado_o_no = leer_json("esta_cifrado_o_no.json")
+
 def cifrador(cosa_que_queremos_cifrar): # Si es una imagen no hay que tocarlo, si es un mensaje hay que hacerle .encode() antes de entrar a la función
     # Preparar la clave y el cifrador AES en modo CBC (más seguro que ECB)
     key = b'\xec\x13x\xa2z\xc7\x8e@>\x1b\xaa\r\x84\x03\x1c\x05V\x95\x80\xda\nN\xed\x1fbk\xf1z\n\x05tN'[:32]  # Asegurar que sea de 256 bits
@@ -50,6 +54,19 @@ def decifrador(cosa_que_queremos_descifrar, key):
     #no hace falta padding porque tiene longitud 32
     return KEY_descifrada_licencia
 
+def calcular_entropia(datos):
+    """
+    Calcula la entropía de los datos proporcionados.
+    :param datos: Bytes del archivo o mensaje a analizar.
+    :return: Entropía calculada.
+    """
+    # Contar la frecuencia de cada byte en los datos
+    frec = Counter(datos)
+    total_bytes = len(datos)
+
+    # Calcular la entropía
+    entropia = -sum((freq / total_bytes) * math.log2(freq / total_bytes) for freq in frec.values())
+    return entropia
 
 def escribir():  # Crea una función para escribir
     while True:
@@ -60,94 +77,83 @@ def escribir():  # Crea una función para escribir
             s_contenidos.close()
             break
         s_contenidos.send(message.encode())  # Enviar mensaje al servidor
-        
-        #print(message[24:])
-        
-        
+
 def escuchar():
-    print("Escribe:")
-    file_bytes= b""
-    file_bytes_cdm=b""
-    archivo_cifrado=""
-    procesar_imagen= "apagado"
+    print(" Escribe el nombre del archivo o busca los disponibles (catalogo):")
+    file_bytes = b""
+    file_bytes_cdm = b""
+    archivo_cifrado = ""
+    procesar_imagen = "apagado"
     global pedir_solicitud_cdm
-    
+
     while True:        
         # Recibir mensaje del servidor
         data = s_contenidos.recv(1024)
-        
-        
-        identificador_principio= data[:11]
-        identificador_final= data[-5:]
-        
-        
+
+        identificador_principio = data[:11]
+        identificador_final = data[-5:]
+
         if identificador_principio == b"<CONTENIDO>":
-            procesar_imagen= "encendido"
-            
-            
+            procesar_imagen = "encendido"
+        
         elif identificador_principio != b"<CONTENIDO>" and procesar_imagen == "encendido":
-            
 
             if identificador_final == b'<FIN>':
-                
                 file_bytes += data[:-5]
                 file_bytes_cdm += data
 
-                if len(file_bytes)%16==0:
-                    archivo_cifrado="si"
-                else:
-                    archivo_cifrado="no"
-                    
-                                            
+                # Calcular la entropía del archivo
+                entropia = calcular_entropia(file_bytes)
+                print(f"Entropía del archivo recibido: {entropia:.4f}")
 
-                print(message[24:]+ " recibid@ \n")
+                if len(file_bytes) % 16 == 0 and entropia>7.5:
+                    archivo_cifrado = "si"
+                else:
+                    archivo_cifrado = "no"
+
+                print(message[24:] + " recibid@ \n")
                 print(f"El archivo {archivo_cifrado} está cifrado")
-                
-                if archivo_cifrado=="si":
-                    identificador_contenido= "<"+message[24:]+">"
-                    pedir_solicitud_cdm="El archivo si esta cifrado "+identificador_contenido
-                    
-                    pedir_solicitud_cdm= cifrador(pedir_solicitud_cdm.encode())
-                    print("Mensaje cifrado ",pedir_solicitud_cdm, "\n")
-                    
+
+                if archivo_cifrado == "si":
+                    identificador_contenido = "<" + message[24:] + ">"
+                    pedir_solicitud_cdm = "El archivo si esta cifrado " + identificador_contenido
+
+                    pedir_solicitud_cdm = cifrador(pedir_solicitud_cdm.encode())
+                    print("Mensaje cifrado ", pedir_solicitud_cdm, "\n")
+
                     CDM.send(pedir_solicitud_cdm)
-                    firma= CDM.recv(1024)
-                    print("Firma: ",firma,"\n")
-                    
-                    
+                    firma = CDM.recv(1024)
+                    print("Firma: ", firma, "\n")
+
                     s_licencias.send(firma)
-                    clave_licencia= s_licencias.recv(1024)
-                    print("Clave licencia: ",clave_licencia)
+                    clave_licencia = s_licencias.recv(1024)
+                    print("Clave licencia: ", clave_licencia)
                     CDM.send(clave_licencia)
                     time.sleep(1) #Pequeño retraso para que no se solapen datos en el CDM
-                    
+
                     CDM.send(file_bytes_cdm)
-                    
-                if archivo_cifrado=="no":
-                    with open('carpeta_del_cliente/contenido_recibido_'+message[24:], 'wb') as file:
+
+                if archivo_cifrado == "no":
+                    with open('carpeta_del_cliente/contenido_recibido_' + message[24:], 'wb') as file:
                         file.write(file_bytes)
-                    identificador_contenido= "<"+message[24:]+">"
+                    identificador_contenido = "<" + message[24:] + ">"
                     no_cifrado = "El archivo no esta cifrado " + identificador_contenido
                     no_cifrado = cifrador(no_cifrado.encode())
                     CDM.send(no_cifrado)
-  
-                print("-"*40+"\n Sigue escribiendo: \n")
-                procesar_imagen= "apagado"
-              
-                file_bytes= b"" #Es necesario porque si no muestra la misma imagen al pedir otras (no se por que)
-                file_bytes_cdm= b""
-                
+
+                print("-" * 40 + "\n Sigue escribiendo: \n")
+                procesar_imagen = "apagado"
+
+                file_bytes = b"" #Es necesario porque si no muestra la misma imagen al pedir otras (no se por que)
+                file_bytes_cdm = b""
+
                 False
-            
+
             else:
                 file_bytes += data
                 file_bytes_cdm += data
-                
 
-            
-            
-            
-        elif procesar_imagen=="apagado":
+        elif procesar_imagen == "apagado":
             key = b'\xec\x13x\xa2z\xc7\x8e@>\x1b\xaa\r\x84\x03\x1c\x05V\x95\x80\xda\nN\xed\x1fbk\xf1z\n\x05tN'[:32]  # Asegurarse de que sea de 256 bits
 
             # Descifrar el contenido
@@ -157,11 +163,9 @@ def escuchar():
             unpadder = padding.PKCS7(128).unpadder()
             mensaje_despadding = unpadder.update(mensaje_descifrado) + unpadder.finalize()
 
-            print("\n Mensaje recibido: \n", mensaje_despadding.decode(),"\n")
-            print("-"*40+"\n Sigue escribiendo:")
-        
-    
-        
+            print("\nMensaje recibido: \n", mensaje_despadding.decode(), "\n")
+            print("-" * 40 + "\n Sigue escribiendo:")
+
 # Crear el socket TCP
 s_contenidos = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s_contenidos.connect(("127.0.0.1", 6001))
